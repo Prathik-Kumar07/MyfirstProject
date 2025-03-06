02b3a2cce19d479ee27d06b3344ca7f80cc8ffb4
Jeet <pranavjeet99@gmail.com>
2025-03-06 09:36:11 +0530

    Initial commit

---
+++ src/test/java/com/example/FirstunderstandingSpring/FirstunderstandingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstunderstandingSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstunderstandingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
